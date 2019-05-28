a3c5316bf040088d968b1015eafd4246376d7398
Mendelt <men.luinenburg@gmail.com>
2019-05-28 13:39:45 +0200

    Adding POJOs

---
+++ src/main/java/nl/subsequence/spring/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package nl.subsequence.spring.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/nl/subsequence/spring/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package nl.subsequence.spring.petclinic.model;
+
+public class Vet extends Person {
+}
